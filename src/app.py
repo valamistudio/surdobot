0f6524b7e1e5ad9e0ea55f88a577f7f00fd15970
Bruno Logerfo <bclogerfo@hotmail.com>
2022-12-07 02:42:32 -0300

    update

--- src/app.py
+++ src/app.py
@@ -1,17 +1,45 @@
+from __future__ import annotations
 from chalice.app import Chalice
 import os
 from chalicelib import file_utils, bot_utils
+import telebot
+from retry import retry
 
 wit_token = os.environ.get('wit_token')
+if wit_token is None:
+    print('wit_token is not assigned')
+    exit()
 
 app = Chalice(app_name='surdobot')
 
-@app.route('/webhook', methods=['POST'])
-def webhook() -> None:
+@retry(tries = 3)
+def respond(file: str, chat_id, message_id, reply: telebot.types.Message) -> telebot.types.Message | None:
     import json
     import requests
     import re
+    with open(file, 'rb') as f:
+        resp = requests.post(
+            #?v=20221114
+            #&context=%7B%22locale%22%3A%22pt_BR%22%7D
+            'https://api.wit.ai/dictation',
+            headers = {
+                'Content-Type': file_utils.CONTENT_TYPE,
+                'Authorization': f'Bearer {wit_token}'
+            },
+            data = f,
+            stream = True)
 
+    bytes = resp.raw.read()
+    content = bytes.decode(json.detect_encoding(bytes))
+    data = re.split(r'(?<=\})\s*(?=\{)', content)
+    jsons = [json.loads(item) for item in data]
+    finals = [obj.get('text') for obj in jsons if obj.get('is_final')]
+    text = ' '.join(finals)
+    print(f'Transcribed text: {text}')
+    return bot_utils.append_message(reply, chat_id, message_id, text)
+
+@app.route('/webhook', methods=['POST'])
+def webhook() -> None:
     request = app.current_request
     if request is None:
         return
@@ -46,34 +74,11 @@ def webhook() -> None:
         return
 
     for file in files:
-        for i in range(3): #attempts
-            try:
-                print(f'Attempt #{i + 1}...')
-                with open(file, 'rb') as f:
-                    resp = requests.post(
-                        #?v=20221114
-                        #&context=%7B%22locale%22%3A%22pt_BR%22%7D
-                        'https://api.wit.ai/dictation',
-                        headers = {
-                            'Content-Type': file_utils.CONTENT_TYPE,
-                            'Authorization': f'Bearer {wit_token}'
-                        },
-                        data = f,
-                        stream = True)
-
-                bytes = resp.raw.read()
-                content = bytes.decode(json.detect_encoding(bytes))
-                data = re.split(r'(?<=\})\s*(?=\{)', content)
-                jsons = [json.loads(item) for item in data]
-                finals = [obj.get('text') for obj in jsons if obj.get('is_final')]
-                text = ' '.join(finals)
-                print(f'Transcribed text: {text}')
-                reply = bot_utils.append_message(reply, chat_id, message_id, text)
-                break
-            except Exception as e:
-                print(e)
-                pass
-
-        bot_utils.delete_message(reply)
+        new_reply = respond(file, chat_id, message_id, reply)
+        if new_reply:
+            reply = new_reply
+        else:
+            bot_utils.delete_message(reply)
+            return
 
     bot_utils.commit_message(reply)
