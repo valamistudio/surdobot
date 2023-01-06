d03fd0f0e2129551d7c9a902485fcaaea17acc6a
Bruno Logerfo <bclogerfo@hotmail.com>
2023-01-06 20:55:13 -0300

    aborting transcription if wit.ai errors

--- src/app.py
+++ src/app.py
@@ -33,12 +33,13 @@ def respond(file: str, chat_id: int, message_id: int, reply: telebot.types.Messa
     content = bytes.decode(json.detect_encoding(bytes))
     data = re.split(r'(?<=\})\s*(?=\{)', content)
     jsons = [json.loads(item) for item in data]
-    errors = [obj.get('error') for obj in jsons]
-    errors = [error for error in errors if error is not None]
+    errors = [obj.get('error') for obj in jsons if obj.get('error') is not None]
     if any(errors):
         print('Wit.AI Errors:')
         for error in errors:
             print(error)
+
+        return
 
     finals = [obj.get('text') for obj in jsons if obj.get('is_final')]
     text = ' '.join(finals)
